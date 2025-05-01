b351bff65cc186bdc339e95637af82f4852e3b24
irdinsmore1 <irdinsmore1@geisinger.edu>
2025-05-01 10:50:34 -0400

    finished aorta extraction

--- src/airc_extract/airc_report.py
+++ src/airc_extract/airc_report.py
@@ -64,10 +64,11 @@ class AircReport:
                 logger.error(f"Failed to read {self.dicom_dir / dicom}: {e}")
         self.dicom_data = valid_dicoms
 
-    def extract_report(self):
+    def extract_report(self) -> dict:
         """Extract the report data from the dicom files"""
         self.validate_identifiers()
         self.extract_measurements()
+        return self.report_data
 
     def validate_identifiers(self):
         """validate that the identifiers are present in the dicom data and are equal"""
@@ -124,33 +125,53 @@ class AircReport:
             raise ContentMissingError("No ContentSequence found in DICOM data")
 
         content = data.ContentSequence
-        id_content = content[0]
-        if not hasattr(id_content, "ConceptCodeSequece"):
-            logger.error(f"No AIRC Code found in {data.filename}")
-            raise ContentMissingError("No AIRC Code found in DICOM data")
-        # Match the code to the AIRC code map
-        code = id_content.ConceptCodeSequece[0].CodeValue
-        if code not in self.code_map:
-            logger.error(f"Code {code} not found in AIRC code map")
-            raise ContentMissingError("Code not found in AIRC code map")
-        # This is one of the 6 AIRC measurements done - will be the key for the output dictionary
-        measurement = self.code_map[code]
+        measurement = self._match_code_to_airc_measurement(content)
+        measure_content = self._get_measurement_content_sequence(content)
         # Get the measurement data
         match measurement:
             case "lung_parenchyma":
-                measures = self._extract_lung_parenchyma_measurements(content)
+                measures = self._extract_lung_parenchyma_measurements(measure_content)
             case "coronary_calcium":
-                measures = self._extract_coronary_calcium_measurements(content)
+                measures = self._extract_coronary_calcium_measurements(measure_content)
             case "aortic_diameters":
-                measures = self._extract_aortic_diameter_measurements(content)
+                measures = self._extract_aortic_diameter_measurements(measure_content)
             case "spine_measurements":
-                measures = self._extract_spine_measurements(content)
+                measures = self._extract_spine_measurements(measure_content)
             case "pulmonary_densities":
-                measures = self._extract_pulmonary_density_measurements(content)
+                measures = self._extract_pulmonary_density_measurements(measure_content)
             case "lung_lesions":
-                measures = self._extract_lung_lesion_measurements(content)
+                measures = self._extract_lung_lesion_measurements(measure_content)
    
         return measurement, measures
+
+    def _get_measurement_content_sequence(self, content):
+        image_measure_code = '126010'
+        measure_content = None
+        for seq in content:
+            if seq.ConceptNameCodeSequence[0].CodeValue == image_measure_code:
+                # This is the image measure - we want to extract the data from this
+                measure_content = seq
+                break
+        if not measure_content:
+            logger.error(f"No image measure sequence found")
+            raise ContentMissingError("No image measure found in DICOM data")
+        return measure_content
+
+    @staticmethod
+    def _match_code_to_airc_measurement(content):
+        id_content = content[0]
+        code_map = AircReport.code_map
+        if not hasattr(id_content, "ConceptCodeSequence"):
+            logger.error(f"No AIRC Code found")
+            raise ContentMissingError("No AIRC Code found in DICOM data")
+        # Match the code to the AIRC code map
+        code = id_content.ConceptCodeSequence[0].CodeValue
+        if code not in code_map:
+            logger.error(f"Code {code} not found in AIRC code map")
+            raise ContentMissingError("Code not found in AIRC code map")
+        # This is one of the 6 AIRC measurements done - will be the key for the output dictionary
+        measurement = code_map[code]
+        return measurement
 
     def _extract_aortic_diameter_measurements(self, content: dcm.DataElement) -> dict:
         """Extract the aortic diameters from the dicom data
@@ -159,11 +180,43 @@ class AircReport:
         """
         # Get the measurements
         location_code_map = {
-            'CHESTCT0408': 'asc_max',
-            'CHESTCT0409': 'desc_max',
-            'C33557': 'sinus_of_valsalva'
+            'CHESTCT0408': 'max_ascending',
+            'CHESTCT0409': 'max_descending',
+            'C33557': 'sinus_of_valsalva',
+            'RID579': 'sinotubular_junction',
+            'CHESTCT0401': 'mid_ascending',
+            'CHESTCT0402': 'proximal_arch',
+            'CHESTCT0403': 'mid_arch',
+            'CHESTCT0404': 'proximal_descending',
+            'CHESTCT0405': 'mid_descending',
+            'CHESTCT0406': 'diaphragm_level',
+            'RID905': 'celiac_artery_origin',
         }
-        pass
+        diameters = {}
+        aorta_measures = content.ContentSequence
+        for measure in aorta_measures:
+            # Each measure is itself a sequence of data describing where the measure is taken and the value
+            measure_content = measure.ContentSequence
+            finding_site_sequence = '363698007'
+            diameter_sequence = 'RID13432'
+            site_location = None
+            diameter = None
+            # Loop through the sequences to pull out the location and the diameter
+            for sequence in measure_content:
+                seq_code = sequence.ConceptNameCodeSequence[0].CodeValue
+                if seq_code == finding_site_sequence:
+                    site_code = sequence.ConceptCodeSequence[0].CodeValue
+                    # This is just the final code for PACS - not a meausurement
+                    if site_code == 'RID480':
+                        continue
+                    site_location = location_code_map.get(site_code, f'{sequence.ConceptCodeSequence[0].CodeValue}, {sequence.ConceptCodeSequence[0].CodeMeaning}')
+                if seq_code == diameter_sequence:
+                    # This is the measurement
+                    diameter = int(sequence.MeasuredValueSequence[0].NumericValue)
+            # If we have both the location and the diameter, add it to the dictionary
+            if site_location and diameter:
+                diameters[site_location] = diameter
+        return diameters
 
     def _extract_lung_parenchyma_measurements(self, content: dcm.DataElement) -> dict:
         """Extract the lung parenchyma measurements from the dicom data
